d4afe882874430f78e3d7553a42577f7aa73f260
Rajnish Singh <rajnishsingh9451270332@gmail.com>
2025-02-16 22:21:18 +0530

    commit

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ import joblib
 heart_model = joblib.load("model/heart.pkl")  # Correct way to load
 
 diabetes_model = pickle.load(open("model/diabetes.pkl", 'rb'))
-kidney_model = joblib.load("model/kidney.pkl")
+with open("model/kidney.pkl", "rb") as file:
+    loaded_model = pickle.load(file)
 # Add Breast Cancer Model Loading
 breast_cancer_model = pickle.load(open("model/breastcancer.pkl", 'rb'))
 
